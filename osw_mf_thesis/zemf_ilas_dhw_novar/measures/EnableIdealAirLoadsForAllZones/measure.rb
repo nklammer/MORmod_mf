b802b58a2e01c4cdbbb8139d6ab08ebe8d139c83
nklammer <noah.klammer@nrel.gov>
2021-06-22 11:27:20 -0600

    New measure didn't work, still want to upload progress

--- osw_mf_thesis/zemf_ilas_dhw_novar/measures/EnableIdealAirLoadsForAllZones/measure.rb
+++ osw_mf_thesis/zemf_ilas_dhw_novar/measures/EnableIdealAirLoadsForAllZones/measure.rb
@@ -69,22 +69,21 @@ class EnableIdealAirLoadsForAllZones < OpenStudio::Measure::ModelMeasure
     # array of zones initially using ideal air loads
     startingIdealAir = []
 
-    runner.registerInfo("The first ten Zone HVAC Components are #{model.getZoneHVACComponents[0..9]}.")
-    runner.registerInfo("The first ten AirLoopHVACs are #{model.getAirLoopHVACs[0..9]}.")
-    runner.registerInfo("The first ten PlantLoops are #{model.getPlantLoops[0..9]}.")
-
     # # remove zone equipment except for exhaust and natural ventilation
     zone_hvac_ideal_found = false
+
     # # is the Zone HVACComponents removing PlantEquipmentOperationSchemes?
     # model.getZoneHVACComponents.each do |component|
     #   next if component.to_FanZoneExhaust.is_initialized
     #   component.remove
     # end
 
+    ideal_systems = 0
+    output_vars_added = 0
     thermalZones = model.getThermalZones
     thermalZones.each do |zone|
       # TODO: - need to also look for ZoneHVACIdealLoadsAirSystem
-      if zone.useIdealAirLoads
+      if zone.useIdealAirLoads # indicates if the zone will use EnergyPlus ideal air loads; may not work
         startingIdealAir << zone
       else
 
@@ -92,40 +91,64 @@ class EnableIdealAirLoadsForAllZones < OpenStudio::Measure::ModelMeasure
           startingIdealAir << zone
         else
 
-          next if !zone.thermostatSetpointDualSetpoint.is_initialized
+          next if !zone.thermostatSetpointDualSetpoint.is_initialized # skip the zone loop if a thermostat is not present in the zone
           ideal_loads = OpenStudio::Model::ZoneHVACIdealLoadsAirSystem.new(model)
-          ideal_loads.addToThermalZone(zone)
+          ideal_loads.addToThermalZone(zone) # ZoneHVACComponent class
           # Set the ideal loads properties
-          # ideal_loads.setMinimumCoolingTemperature(0.0)
+          ideal_loads.setName("#{zone.name} ILAS")
+          # ideal_loads.setHeatingLimit(40.0)
+          # ideal_loads.setMinimumCoolingSupplyAirTemperature(0.0)
         end
+
+        # outside checking if thermostat exists for zone
+        # add output variable
+        ideal_loads_air_system_variables = [
+          'Zone Ideal Loads Zone Total Cooling Energy'
+        ]
+
+        ideal_loads_air_system_variables.each do |variable|
+          # create the OutputVariable definition
+          output_var = OpenStudio::Model::OutputVariable.new(variable, model)
+          output_var.setKeyValue("#{zone.name}") # must be simple string
+          output_var.setReportingFrequency('hourly')
+          output_var.setName("#{zone.name} ILAS Total Cooling Energy") # this name might not make it into IDF
+          runner.registerInfo("Adding output variable for '#{output_var.variableName}' reporting '#{output_var.reportingFrequency}''.")
+          runner.registerInfo("Key value for variable is '#{output_var.keyValue}'. I've named it '#{output_var.name}'.")
+
+          output_vars_added += 1
+        end
+
+        ideal_systems += 1
+
+
 
       end
     end
 
-    # remove air and plant loops not used for SWH
-    model.getAirLoopHVACs.each(&:remove)
+    # # remove air and plant loops not used for SWH
+    # model.getAirLoopHVACs.each(&:remove)
 
     # see if plant loop is swh or not and take proper action (booter loop doesn't have water use equipment)
     # for this experiment, swh will be included in ideal air loads
-    model.getPlantLoops.each do |plant_loop|
-      is_swh_loop = false
-      plant_loop.supplyComponents.each do |component|
-        # the supply components consist of
-        # Nodes, Pipes, Pumps, Splitters, Mixers       
-        if component.to_WaterHeaterMixed.is_initialized ||
-           component.to_WaterHeaterStratified.is_initialized ||
-           component.to_WaterHeaterHeatPump.is_initialized ||
-           component.to_WaterHeaterHeatPumpWrappedCondenser.is_initialized
-          is_swh_loop = true # this variable stays set after the loop until otherwise reset
-          next # skips to next iteration of |component| block
-          # if one SupplyComponent is not a water heater, the 
-          # is_swh_loop value may be left at 'false' and the loop may be removed!
-        end
-      end
-      if is_swh_loop == false
-        plant_loop.remove
-      end
-    end
+    # model.getPlantLoops.each do |plant_loop|
+    #   is_swh_loop = false
+    #   plant_loop.supplyComponents.each do |component|
+    #     # the supply components consist of
+    #     # Nodes, Pipes, Pumps, Splitters, Mixers       
+    #     if component.to_WaterHeaterMixed.is_initialized ||
+    #        component.to_WaterHeaterStratified.is_initialized ||
+    #        component.to_WaterHeaterHeatPump.is_initialized ||
+    #        component.to_WaterHeaterHeatPumpWrappedCondenser.is_initialized
+    #       is_swh_loop = true # this variable stays set after the loop until otherwise reset
+    #       next # skips to next iteration of |component| block
+    #       # if one SupplyComponent is not a water heater, the 
+    #       # is_swh_loop value may be left at 'false' and the loop may be removed!
+    #     end
+    #   end
+    #   if is_swh_loop == false
+    #     plant_loop.remove
+    #   end
+    # end
 
     # reporting initial condition of model
     runner.registerInitialCondition("In the initial model #{startingIdealAir.size} zones use ideal air loads.")
@@ -133,11 +156,11 @@ class EnableIdealAirLoadsForAllZones < OpenStudio::Measure::ModelMeasure
     # reporting final condition of model
     finalIdealAir = []
     thermalZones.each do |zone|
-      if zone.useIdealAirLoads
+      if zone.useIdealAirLoads # this method may not work. it is not used in standards method model_add_ideal_air_loads
         finalIdealAir << zone
       end
     end
-    runner.registerFinalCondition("In the final model #{finalIdealAir.size} zones use ideal air loads.")
+    runner.registerFinalCondition("The measure added #{ideal_systems} ideal load air systems and #{output_vars_added} output variables.")
 
     return true
   end
